4ab59cb9889c46361afa70fd6fdd349a019a8cdb
Steven Saunders <steven@supersuperstore.com>
2021-06-19 19:10:04 +0100

    added POST, CREATE, PUT  and DELETE methods to Property_Listing

--- project/settings.py
+++ project/settings.py
@@ -130,8 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': (
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
--- real_estate/views.py
+++ real_estate/views.py
@@ -16,16 +16,41 @@ class PropertyListingListView(APIView):
         property_listings = Property_Listing.objects.all()
         serialized_property_listings = PropertyListingSerializer(property_listings, many=True)
         return Response(serialized_property_listings.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        new_property_listing = PropertyListingSerializer(data=request.data)
+        if new_property_listing.is_valid():
+            new_property_listing.save()
+            return Response(new_property_listing.data, status=status.HTTP_201_CREATED)
+        return Response(new_property_listing.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 class PropertyListingDetailView(APIView):
-    
-    def get(self, _request, pk):
+
+    def get_Property_Listing(self, pk):
         try:
-            property_listing = Property_Listing.objects.get(pk=pk)
-            serialized_property_listing = PropertyListingSerializer(property_listing)
-            return Response(serialized_property_listing.data, status=status.HTTP_200_OK)
+            return Property_Listing.objects.get(pk=pk)
         except Property_Listing.DoesNotExist:
             raise NotFound()
+    
+    def get(self, _request, pk):
+        property_listing = self.get_Property_Listing(pk=pk)
+        serialized_property_listing = PropertyListingSerializer(property_listing)
+        return Response(serialized_property_listing.data, status=status.HTTP_200_OK)
+        
+
+    def delete(self, _request, pk):
+        property_listing_to_delete = self.get_Property_Listing(pk=pk)
+        property_listing_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        property_listing_to_update = self.get_Property_Listing(pk=pk)
+        updated_property_listing = PropertyListingSerializer(property_listing_to_update, data=request.data)
+        if updated_property_listing.is_valid():
+            updated_property_listing.save()
+            return Response(updated_property_listing.data, status=status.HTTP_202_ACCEPTED)
+        return Response(updated_property_listing.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 
 # class PropertyListingListView(View):
