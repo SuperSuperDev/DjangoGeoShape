9517fbbf467da82700ddeee9f16d4908c4e1fc62
Steven Saunders <steven@supersuperstore.com>
2021-06-19 18:13:03 +0100

    Added get single property listing endpoint

--- real_estate/views.py
+++ real_estate/views.py
@@ -3,18 +3,30 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 # from django.views import View
 
 # Create your views here.
 
 from .models import Property_Listing
-from .serializers import PropertyListingSerializer
+from .serializers import PropertyListingSerializer 
 class PropertyListingListView(APIView):
 
     def get(self, _request):
         property_listings = Property_Listing.objects.all()
         serialized_property_listings = PropertyListingSerializer(property_listings, many=True)
         return Response(serialized_property_listings.data, status=status.HTTP_200_OK)
+
+class PropertyListingDetailView(APIView):
+    
+    def get(self, _request, pk):
+        try:
+            property_listing = Property_Listing.objects.get(pk=pk)
+            serialized_property_listing = PropertyListingSerializer(property_listing)
+            return Response(serialized_property_listing.data, status=status.HTTP_200_OK)
+        except Property_Listing.DoesNotExist:
+            raise NotFound()
+
 
 # class PropertyListingListView(View):
 
